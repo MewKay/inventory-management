8a0d8acfa7f55fd9be324878609edda6f71b4084
MewKay <miorakanto25z@gmail.com>
2025-01-27 15:24:50 +0300

    Setup ejs

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index");
 });
 
 module.exports = indexRouter;
