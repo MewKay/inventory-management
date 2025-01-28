0750f3b53c61bc114337712a0683d686efe6c859
MewKay <miorakanto25z@gmail.com>
2025-01-28 15:44:54 +0300

    Add order direction to query

--- controllers/productListController.js
+++ controllers/productListController.js
@@ -1,9 +1,34 @@
 const { getAllProducts } = require("../db/queries");
 
+const getColumnDirection = function getColumnDirectionIfSortedBy(
+  columnName,
+  currentSort,
+  currentDirection,
+) {
+  if (columnName !== currentSort) {
+    return "";
+  }
+
+  if (currentDirection === "asc") {
+    return "desc";
+  }
+
+  if (currentDirection === "desc") {
+    return "asc";
+  }
+};
+
 const productsGet = async (req, res) => {
-  const { sort } = req.query;
-  const products = await getAllProducts(sort);
-  res.render("productTable", { products: products });
+  const { sort, direction } = req.query;
+  const products = await getAllProducts(sort, direction);
+
+  res.render("productTable", {
+    products: products,
+    nameDirection: getColumnDirection("name", sort, direction),
+    quantityDirection: getColumnDirection("quantity", sort, direction),
+    priceDirection: getColumnDirection("price", sort, direction),
+    categoryDirection: getColumnDirection("category", sort, direction),
+  });
 };
 
 module.exports = { productsGet };
--- db/queries.js
+++ db/queries.js
@@ -1,16 +1,25 @@
 const pool = require("./pool");
 
-const getAllProducts = async function queryAllProductsDataFromDB(sort) {
-  // Escape parameter before entering query
+const getAllProducts = async function queryAllProductsDataFromDB(
+  sort,
+  direction = "ASC",
+) {
+  // Escape parameters before entering query
   const validParameters = ["name", "quantity", "price", "category"];
   const columnToSortBy = validParameters.includes(sort) ? sort : "id";
+
+  const validDirections = ["ASC", "DESC"];
+  const upperCasedDirection = direction.toUpperCase();
+  const sortDirection = validDirections.includes(upperCasedDirection)
+    ? upperCasedDirection
+    : "ASC";
 
   const query = `
     SELECT p.id, p.name, quantity, unit, price, c.name AS category
     FROM product p
     INNER JOIN category c 
       ON p.category_id = c.id
-    ORDER BY ${columnToSortBy};
+    ORDER BY ${columnToSortBy} ${sortDirection};
     `;
 
   const { rows } = await pool.query(query);
