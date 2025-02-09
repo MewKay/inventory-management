9ae14817979639bc76d5c82d42b9686b858f98ae
MewKay <miorakanto25z@gmail.com>
2025-02-09 15:49:57 +0300

    Update productList controller
    Used express-validator for the controller.
    Removed the obsolete validateTableQueryParams.

--- controllers/productListController.js
+++ controllers/productListController.js
@@ -1,33 +1,32 @@
 const { getAllProducts, getTotalProductsCount } = require("../db/queries");
-const validateTableQueryParams = require("../utils/validateTableQueryParams");
 const getCurrentColumnDirection = require("../utils/getCurrentColumnDirection");
 const { createPagination } = require("../utils/pagination");
+const validateTableQueryParams = require("../middlewares/validators/validateTableQueryParams");
+const { matchedData } = require("express-validator");
 
-const productsGet = async (req, res) => {
-  const { sort, direction } = req.query;
-  const totalProducts = await getTotalProductsCount();
-  const pagination = createPagination(req.query, totalProducts);
+const productsGet = [
+  validateTableQueryParams,
+  async (req, res) => {
+    const { sort, direction } = matchedData(req);
+    const totalProducts = await getTotalProductsCount();
+    const pagination = createPagination(req.query, totalProducts);
 
-  const { columnToSortBy, sortDirection } = await validateTableQueryParams(
-    sort,
-    direction,
-  );
+    const products = await getAllProducts(
+      sort,
+      direction,
+      pagination.productsPerPage,
+      pagination.offset,
+    );
 
-  const products = await getAllProducts(
-    columnToSortBy,
-    sortDirection,
-    pagination.productsPerPage,
-    pagination.offset,
-  );
-
-  res.render("productList", {
-    products: products,
-    pagination: pagination,
-    nameDirection: getCurrentColumnDirection("name", sort, direction),
-    quantityDirection: getCurrentColumnDirection("quantity", sort, direction),
-    priceDirection: getCurrentColumnDirection("price", sort, direction),
-    categoryDirection: getCurrentColumnDirection("category", sort, direction),
-  });
-};
+    res.render("productList", {
+      products: products,
+      pagination: pagination,
+      nameDirection: getCurrentColumnDirection("name", sort, direction),
+      quantityDirection: getCurrentColumnDirection("quantity", sort, direction),
+      priceDirection: getCurrentColumnDirection("price", sort, direction),
+      categoryDirection: getCurrentColumnDirection("category", sort, direction),
+    });
+  },
+];
 
 module.exports = { productsGet };
