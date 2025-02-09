c5a87756ba18fca5dc651c33f82e82011022c8c2
MewKay <miorakanto25z@gmail.com>
2025-02-09 21:15:51 +0300

    Add validator to productEdit controller

--- controllers/productEditController.js
+++ controllers/productEditController.js
@@ -1,15 +1,26 @@
+const { validationResult, matchedData } = require("express-validator");
 const { getProductDetails, getAllCategories } = require("../db/queries");
+const validateProductParam = require("../middlewares/validators/validateProductParam");
 
-const productEditGet = async (req, res) => {
-  const productId = parseInt(req.params.productId);
-  const product = await getProductDetails(productId);
-  const categories = await getAllCategories();
+const productEditGet = [
+  validateProductParam,
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  res.render("productEdit", {
-    title: "Edit Product",
-    product: product,
-    categories: categories,
-  });
-};
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array()[0].msg);
+    }
+
+    const { productId } = matchedData(req);
+    const product = await getProductDetails(productId);
+    const categories = await getAllCategories();
+
+    res.render("productEdit", {
+      title: "Edit Product",
+      product: product,
+      categories: categories,
+    });
+  },
+];
 
 module.exports = { productEditGet };
