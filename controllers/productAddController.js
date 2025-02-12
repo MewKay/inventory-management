19116c14f63384aaebf5ab97253964e1afe0f0b4
MewKay <miorakanto25z@gmail.com>
2025-02-12 15:36:22 +0300

    Create product add view

--- controllers/productAddController.js
+++ controllers/productAddController.js
@@ -1,6 +1,23 @@
+const { getAllCategories } = require("../db/queries");
+
+const emptyFieldsProduct = {
+  name: "",
+  quantity: "",
+  unit: "",
+  price: "",
+  category_id: "",
+};
+
 const productAddGet = [
-  (req, res) => {
-    res.send("Hello Product Add View!");
+  async (req, res) => {
+    const categories = await getAllCategories();
+
+    res.render("productAdd", {
+      title: "Add new product",
+      product: emptyFieldsProduct,
+      categories: categories,
+      errors: [],
+    });
   },
 ];
 
