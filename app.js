d4e61b6e3ba67b201541d2926cae71b1034d863b
MewKay <miorakanto25z@gmail.com>
2025-02-13 18:39:55 +0300

    Create general error handling middleware

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ const productListCategoryRouter = require("./routes/productListCategory.router")
 const productDetailsRouter = require("./routes/productDetails.router");
 const productEditRouter = require("./routes/productEdit.router");
 const productAddRouter = require("./routes/productAdd.router");
+const errorHandler = require("./middlewares/errors/errorHandler");
 app.use("/", indexRouter);
 app.use("/view/products", productListRouter);
 app.use("/view/category/:categoryId", productListCategoryRouter);
 app.use("/view/products/:productId", productDetailsRouter);
 app.use("/edit/products/new", productAddRouter);
 app.use("/edit/products/:productId", productEditRouter);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
