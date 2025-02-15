0c6773e77c0a1d0f1d25334dafa3b4c07f1dc722
MewKay <miorakanto25z@gmail.com>
2025-02-15 20:51:55 +0300

    Create general error view

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
 
   console.error(err);
-  res.status(statusCode).send(err.message);
+  res.status(statusCode).render("error", { errorMessage: err.message });
 };
 
 module.exports = errorHandler;
