673cfc5ac751baa4080cd6eec774914dd43ac39a
MewKay <miorakanto25z@gmail.com>
2025-02-19 13:13:39 +0300

    Create head partials
    Put default styles to the head partial to be shown on every view.
    Setup app to handle static files.

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const path = require("node:path");
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
+const viewsPath = path.join(__dirname, "views");
+const assetsPath = path.join(__dirname, "public");
+
+app.set("views", viewsPath);
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 
 const indexRouter = require("./routes/index.router");
 const productListRouter = require("./routes/productList.router");
