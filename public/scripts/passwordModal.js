751c0ce5eef9a4eaa927116fc680438c84f44882
MewKay <miorakanto25z@gmail.com>
2025-02-21 08:57:31 +0300

    Include passwordModal to productEdit
    Updated passwordModal script to isolate each form when multiple are
    present on the same page.

--- public/scripts/passwordModal.js
+++ public/scripts/passwordModal.js
@@ -1,54 +1,59 @@
-// eslint-disable-next-line no-undef
-const dialog = document.querySelector("dialog");
-const form = dialog.closest("form");
-const passwordInput = dialog.querySelector("input");
-const confirmButton = dialog.querySelector(".confirm");
-const cancelButton = dialog.querySelector(".cancel");
+/* eslint-disable no-undef */
+const ProtectedForm = (form) => {
+  const openModalButton = form.querySelector(".open-dialog");
+  const dialog = form.querySelector("dialog");
+  const passwordInput = dialog.querySelector("input");
+  const confirmButton = dialog.querySelector(".confirm");
+  const cancelButton = dialog.querySelector(".cancel");
 
-const openModal = () => {
-  passwordInput.removeAttribute("disabled");
-  dialog.showModal();
-};
+  const openModal = () => {
+    passwordInput.removeAttribute("disabled");
+    dialog.showModal();
+  };
 
-const closeModal = () => {
-  passwordInput.setAttribute("disabled", "");
-  dialog.close();
-};
+  const closeModal = () => {
+    passwordInput.setAttribute("disabled", "");
+    dialog.close();
+  };
 
-const submitForm = (e) => {
-  e.preventDefault();
+  const submitForm = (e) => {
+    e.preventDefault();
 
-  if (!passwordInput.checkValidity()) {
-    openModal();
-    passwordInput.reportValidity();
-    return;
-  }
+    if (!passwordInput.checkValidity()) {
+      openModal();
+      passwordInput.reportValidity();
+      return;
+    }
 
-  if (!form.checkValidity() && passwordInput.checkValidity()) {
-    closeModal();
-    form.reportValidity();
-    return;
-  }
-
-  form.requestSubmit();
-};
-
-cancelButton.addEventListener("click", closeModal);
-confirmButton.addEventListener("click", submitForm);
-
-form.addEventListener("keydown", (e) => {
-  if (e.key === "Enter" && !dialog.open) {
-    if (!form.checkValidity()) {
+    if (!form.checkValidity() && passwordInput.checkValidity()) {
+      closeModal();
       form.reportValidity();
       return;
     }
 
-    openModal();
-  }
-});
+    form.requestSubmit();
+  };
 
-dialog.addEventListener("keydown", (e) => {
-  if (e.key === "Enter" && dialog.open) {
-    submitForm(e);
-  }
-});
+  openModalButton.addEventListener("click", openModal);
+  cancelButton.addEventListener("click", closeModal);
+  confirmButton.addEventListener("click", submitForm);
+
+  form.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && !dialog.open) {
+      if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
+      }
+
+      openModal();
+    }
+  });
+
+  dialog.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && dialog.open) {
+      submitForm(e);
+    }
+  });
+};
+
+document.querySelectorAll("form").forEach((form) => ProtectedForm(form));
